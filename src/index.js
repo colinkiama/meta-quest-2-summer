3d66c7475bfd33200fbb215977d403c8183a6cf7
Colin Kiama <colinkiama@gmail.com>
2022-07-19 20:05:48 +0000

    Started handling scroll event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+let currentURL = new URL(window.location);
+
+console.log("Current URL:", currentURL);
+console.log("Current Hash:", currentURL.hash);
+
+let isHandlingScrollEvent = false;
+
+document.addEventListener("scroll", (evnt) => {
+  if (!isHandlingScrollEvent) {
+    window.requestAnimationFrame(() => {
+      changeHeaderState();
+      isHandlingScrollEvent = false;
+    });
+
+    isHandlingScrollEvent = true;
+  }
+});
+
+function changeHeaderState() {
+  let header = document.querySelector(".header-bar");
+  let elementState = header.getAttribute("data-state");
+}
